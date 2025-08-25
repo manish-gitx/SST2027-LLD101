af2b56e6279eebfe2c1d13ee464e5f393970eeab
manish-gitx <rachakondamanish9@gmail.com>
2025-08-25 06:31:31 +0530

    sensitive case

--- SOLID/ex03/src/Demo03.java
+++ SOLID/ex03/src/Demo03.java
@@ -1,4 +1,4 @@
-public class demo03{
+public class Demo03{
     public static void main(String[] args){
         System.out.println(new shippingcostcalculator().cost(new shipment("EXPRESS",2.0)));
     }
